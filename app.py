52f987b597c5e2425cad791d32060cbdf8091150
Surote Wongpaiboon <surote.z9@gmail.com>
2024-01-03 19:40:21 +0700

    feat: add shutdown

--- app.py
+++ app.py
@@ -34,9 +34,16 @@ def local_api():
     data = {'data': 'Cannot find correct env api_endpoint'}
     return json.dumps(data),200
 
-@app.route('/completed')
-def complete():
-    return 0
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+    
+@app.get('/shutdown')
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8081)
